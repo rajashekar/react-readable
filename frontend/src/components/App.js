42a2f8092c0f416939cc1c69ea3cd75b3bd327b8
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2017-11-26 10:52:26 -0800

    refactor: converting App component to Functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch,Route,withRouter } from 'react-router-dom'
 
 import Categories from './Categories';
@@ -12,23 +12,18 @@ import '../App.css';
  * Main component for this app
  */
 
-class App extends Component {
-
   // Main render method
-  render() {
-    return (
-        <div className='ContentWrapper'>
-            <Switch>
-                <Route exact path="/newpost" component={CreatePost}/>
-                <Route exact path="/editpost/:post_id" component={EditPost}/>
-                <Route exact path="/" component={Categories}/>
-                <Route exact path="/:category" component={Categories}/>
-                <Route exact path="/:category/:post_id" component={PostView}/>
-            </Switch>
-        </div>
-    );
-  }
-}
+const App = () => (
+    <div className='ContentWrapper'>
+        <Switch>
+            <Route exact path="/newpost" component={CreatePost}/>
+            <Route exact path="/editpost/:post_id" component={EditPost}/>
+            <Route exact path="/" component={Categories}/>
+            <Route exact path="/:category" component={Categories}/>
+            <Route exact path="/:category/:post_id" component={PostView}/>
+        </Switch>
+    </div>
+)
 
 // withRouter is needed since some of the Route's are not working
 export default withRouter(App)
